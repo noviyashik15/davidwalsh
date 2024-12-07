397149f869c3983671711526a3c2b91fde66e3e4
ItsMyName44 <Noviyashik15@yandex.ru>
2024-12-08 00:00:57 +0300

    first commit

---
+++ backgr.js
@@ -0,0 +1,96 @@
+const button1 = document.getElementById('butn01');
+button1.addEventListener('click', function() {
+  obnuleniefon();
+  document.getElementById("prim01").style.display = "block";
+});
+const button2 = document.getElementById('butn02');
+button2.addEventListener('click', function() {
+  obnuleniefon();
+  document.getElementById("prim02").style.display = "block";
+});
+const button3 = document.getElementById('butn03');
+button3.addEventListener('click', function() {
+  obnuleniefon();
+  document.getElementById("prim03").style.display = "block";
+});
+const button4 = document.getElementById('butn04');
+button4.addEventListener('click', function() {
+  obnuleniefon();
+  document.getElementById("prim04").style.display = "block";
+});
+const button5 = document.getElementById('butn05');
+button5.addEventListener('click', function() {
+  obnuleniefon();
+  document.getElementById("prim05").style.display = "block";
+});
+const button6 = document.getElementById('butn06');
+button6.addEventListener('click', function() {
+  obnuleniefon();
+  document.getElementById("prim06").style.display = "block";
+});
+const button7 = document.getElementById('butn07');
+button7.addEventListener('click', function() {
+  obnuleniefon();
+  document.getElementById("prim07").style.display = "block";
+});
+const button8 = document.getElementById('butn08');
+button8.addEventListener('click', function() {
+  obnuleniefon();
+  document.getElementById("prim08").style.display = "block";
+});
+const button9 = document.getElementById('butn09');
+button9.addEventListener('click', function() {
+  obnuleniefon();
+  document.getElementById("prim09").style.display = "block";
+});
+const button10 = document.getElementById('butn10');
+button10.addEventListener('click', function() {
+  obnuleniefon();
+  document.getElementById("prim10").style.display = "block";
+});
+const button11 = document.getElementById('butn11');
+button11.addEventListener('click', function() {
+  obnuleniefon();
+  document.getElementById("prim11").style.display = "block";
+});
+const button12 = document.getElementById('butn12');
+button12.addEventListener('click', function() {
+  obnuleniefon();
+  document.getElementById("prim12").style.display = "block";
+});
+const button13 = document.getElementById('butn13');
+button13.addEventListener('click', function() {
+  obnuleniefon();
+  document.getElementById("prim13").style.display = "block";
+});
+const button14 = document.getElementById('butn14');
+button14.addEventListener('click', function() {
+  obnuleniefon();
+  document.getElementById("prim14").style.display = "block";
+});
+const button15 = document.getElementById('butn15');
+button15.addEventListener('click', function() {
+  obnuleniefon();
+  document.getElementById("prim15").style.display = "block";
+});
+
+
+var obnuleniefon = function(){
+  document.getElementById("prim01").style.display = "none";  
+  document.getElementById("prim02").style.display = "none";  
+  document.getElementById("prim03").style.display = "none";  
+  document.getElementById("prim04").style.display = "none";  
+  document.getElementById("prim05").style.display = "none";  
+  document.getElementById("prim06").style.display = "none";  
+  document.getElementById("prim07").style.display = "none";  
+  document.getElementById("prim08").style.display = "none";  
+  document.getElementById("prim09").style.display = "none";  
+  document.getElementById("prim10").style.display = "none";  
+  document.getElementById("prim11").style.display = "none";  
+  document.getElementById("prim12").style.display = "none";  
+  document.getElementById("prim13").style.display = "none";  
+  document.getElementById("prim14").style.display = "none";  
+  document.getElementById("prim15").style.display = "none";  
+
+}
+
